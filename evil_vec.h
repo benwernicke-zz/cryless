160d4eaaca4d13b0d95780eb827d449359130c9f
benwernicke <ben.wernicke@student.hpi.uni-potsdam.de>
2021-12-02 22:45:57 +0100

    _VEC_WRAPPER_SIZE macro added ... sizeof(typeof(xxx)) redundancy fixed

--- evil_vec.h
+++ evil_vec.h
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 // TODO: unconditional jumps ??
-// TODO: fix vec_rem
+//
+#define _VEC_WRAPPER_SIZE sizeof(size_t) * 2 + 1
 
 static size_t vec_size(void* vec);
 static void* vec_create();
@@ -11,22 +12,22 @@ static void vec_free(void* vec);
 typedef char byte;
 
 static void _vec_rem(byte* vec, size_t index, size_t type_size);
-#define vec_rem(vec, index) _vec_rem((byte*)vec, index, sizeof(typeof(*vec)));
+#define vec_rem(vec, index) _vec_rem((byte*)vec, index, sizeof(*vec));
 
-#define vec_reserve(vec_name, val)                                       \
-    {                                                                    \
-        size_t _n_bytes = sizeof(typeof(*vec_name)) * val;               \
-        vec_wrapper_resize(vec_name, sizeof(size_t) * 2 + 1 + _n_bytes); \
+#define vec_reserve(vec_name, val)                                  \
+    {                                                               \
+        size_t _n_bytes = sizeof(*vec_name) * val;                  \
+        vec_wrapper_resize(vec_name, _VEC_WRAPPER_SIZE + _n_bytes); \
     };
 
 // TODO: get rid of macro magic
-#define vec_push(vec, val)                                                                  \
-    {                                                                                       \
-        size_t _val_size = sizeof(typeof(*vec));                                            \
-        if (vec_size(vec) * _val_size + 2 * sizeof(size_t) + 1 >= vec_allocated_bytes(vec)) \
-            vec = vec_wrapper_resize(vec, vec_allocated_bytes(vec) * 2 + _val_size);        \
-        vec[vec_size(vec)] = val;                                                           \
-        ((size_t*)vec_get_wrapper(vec))[0]++;                                               \
+#define vec_push(vec, val)                                                             \
+    {                                                                                  \
+        size_t _val_size = sizeof(*vec);                                               \
+        if (vec_size(vec) * _val_size + _VEC_WRAPPER_SIZE >= vec_allocated_bytes(vec)) \
+            vec = vec_wrapper_resize(vec, vec_allocated_bytes(vec) * 2 + _val_size);   \
+        vec[vec_size(vec)] = val;                                                      \
+        ((size_t*)vec_get_wrapper(vec))[0]++;                                          \
     };
 
 #endif
@@ -56,9 +57,9 @@ static size_t vec_allocated_bytes(void* vec)
 // Con- Destructor
 static void* vec_create()
 {
-    byte* vec_wrapper = (byte*)malloc(sizeof(size_t) * 2 + 1);
-    ((size_t*)vec_wrapper)[0] = 0;                      // size
-    ((size_t*)vec_wrapper)[1] = sizeof(size_t) * 2 + 1; // allocated bytes;
+    byte* vec_wrapper = (byte*)malloc(_VEC_WRAPPER_SIZE);
+    ((size_t*)vec_wrapper)[0] = 0;                 // size
+    ((size_t*)vec_wrapper)[1] = _VEC_WRAPPER_SIZE; // allocated bytes;
     return &vec_wrapper[sizeof(size_t) * 2];
 }
 
